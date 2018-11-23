a1f88560102c95dd641f18e49b1e744c2fede381
Liam H <35095843+ir-3@users.noreply.github.com>
2018-11-23 17:19:45 -0600

    Working on stopping memory leaks. Thanks wand. >kek

--- cogs/information.py
+++ cogs/information.py
@@ -173,7 +173,7 @@ class Information:
         )
         e.add_field(
             name="Ping",
-            value=f"{round(self.bot.latency, 2)}ms"
+            value=f"{round(self.bot.latency * 1000, 2)}ms"
         )
         e.add_field(
             name="CPU Usage",
