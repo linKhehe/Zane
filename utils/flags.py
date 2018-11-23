eb97313009d77597ac0bf1cca7e67a4cfd9e595b
Liam H <35095843+ir-3@users.noreply.github.com>
2018-11-22 19:24:53 -0600

    Adding a custom flag parser.

--- utils/flags.py
+++ utils/flags.py
@@ -1,22 +1,16 @@
-class Flags:
-
-    def __init__(self, *args):
-        self.passed_flags = args
-
-    @property
-    def to_dict(self):
-        ret = {"long flags": {}, "short flags": {}}
-        for flag in self.passed_flags:
-            if flag.startswith('--'):
-                flag = flag.replace("--", "")
-                if "=" in flag:
-                    ret['long flags'].update({flag: flag.split("=")[1]})
-                else:
-                    ret['long flags'].update({flag: True})
-            if flag.startswith('-'):
-                flag = flag.replace("-", "")
-                if "=" in flag:
-                    ret['short flags'].update({flag: flag.split("=")[1]})
-                else:
-                    ret['short flags'].update({flag: True})
-        return ret
+def parse_flags(flags):
+    ret = {"long flags": {}, "short flags": {}}
+    for flag in flags:
+        if flag.startswith('--'):
+            flag = flag.replace("--", "")
+            if "=" in flag:
+                ret['long flags'].update({flag: flag.split("=")[1]})
+            else:
+                ret['long flags'].update({flag: True})
+        if flag.startswith('-'):
+            flag = flag.replace("-", "")
+            if "=" in flag:
+                ret['short flags'].update({flag: flag.split("=")[1]})
+            else:
+                ret['short flags'].update({flag: True})
+    return ret
