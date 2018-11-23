eadc1f870eb95b3a0b996953e5b5610f6ab1cb4d
Liam H <35095843+ir-3@users.noreply.github.com>
2018-11-22 19:40:55 -0600

    Adding a custom flag parser.

--- utils/flags.py
+++ utils/flags.py
@@ -2,13 +2,13 @@ def parse_flags(flags):
     ret = {}
     for flag in flags:
         if flag.startswith('--'):
-            flag = flag.replace("--", "")
+            flag = flag.lstrip("-")
             if "=" in flag:
                 ret.update({flag.split("=")[0][0]: flag.split("=")[1]})
             else:
                 ret.update({flag[0]: True})
         if flag.startswith('-'):
-            flag = flag.replace("-", "")
+            flag = flag.lstrip("-")
             if "=" in flag:
                 ret.update({flag.split("=")[0]: flag.split("=")[1]})
             else:
