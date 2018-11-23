c8eb6e73b16f8ca4566903eaacd09a391dff5f21
Liam H <35095843+ir-3@users.noreply.github.com>
2018-11-23 03:37:53 -0600

    Added utils.format for humanizing time since and dates.

--- utils/format.py
+++ utils/format.py
@@ -3,13 +3,14 @@ import datetime
 import humanize
 
 
-def humanized_time_since(date: datetime.datetime)
+def humanized_time_since(date: datetime.datetime):
     now = datetime.datetime.now()
     seconds_since = (now - date).total_seconds()
 
     ret = humanize.naturaltime(seconds_since)
 
     return ret
+
 
 def humanized_date(date: datetime.datetime):
     return humanize.naturaldate(date)
