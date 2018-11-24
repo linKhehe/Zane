0ffad77900f1178eab5d564b8bb8bbadc4bdf1d9
Liam H <35095843+ir-3@users.noreply.github.com>
2018-11-24 17:09:06 -0600

    Adding DBL support.

---
+++ Zane/cogs/dbl.py
@@ -0,0 +1,25 @@
+import os
+
+import dbl
+import asyncio
+
+
+class DiscordBotsOrgAPI:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = os.environ['DBL_TOKEN']
+        self.dbl_py = dbl.Client(self.bot, self.token)
+        self.bot.loop.create_task(self._update_stats())
+
+    async def _update_stats(self):
+        while True:
+            try:
+                await self.dbl_py.post_server_count()
+            except Exception:
+                pass
+            await asyncio.sleep(1800)
+
+
+def setup(bot):
+    bot.add_cog(DiscordBotsOrgAPI(bot))
