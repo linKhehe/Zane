a862f3b35fb3287f88b3511daa8c1a672db3047b
Liam H <35095843+ir-3@users.noreply.github.com>
2018-11-24 16:42:12 -0600

    Working on wasted command.

--- Zane/cogs/image.py
+++ Zane/cogs/image.py
@@ -131,7 +131,7 @@ class Imaging:
         """
         with WandImage(filename="assets/wasted.png") as wasted:
             with image:
-                image.composite(wasted, -500, -100)
+                image.composite(wasted, -450, -150)
                 ret = image.to_discord_file("wasted.png")
 
         return ret
